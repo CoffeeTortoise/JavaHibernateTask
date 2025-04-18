1bea7e06fb6cfa44db6dfedbcc7f95bda59d484c
CoffeeTortoise <e.8ychkov@yandex.ru>
2025-04-18 19:38:35 +0500

    changes

--- src/main/java/com/coffee/Main.java
+++ src/main/java/com/coffee/Main.java
@@ -3,6 +3,7 @@ package com.coffee;
 public class Main {
 
     public static void main(String[] args) {
-        //
+        UserService service = new UserService();
+        service.start();
     }
 }
